48e2c3f58e4c8bbb671cddb122b1255297268ea0
Durgesh Pushpakar <56768530+durgeshpushpakar@users.noreply.github.com>
2021-10-05 01:08:19 +0530

    Update twoSum.cpp

--- Arrays/twoSum.cpp
+++ Arrays/twoSum.cpp
@@ -4,21 +4,21 @@ vector<int> twoSum(vector<int>& nums, int target) {
     unordered_map<int, int>umap;
     unordered_map<int, int>freq;
     vector<int>ans{0, 0};
-    cout<<"array: ";
+    // cout<<"array: ";
     for(int i=0;i<nums.size();i++){
-        cout<<nums[i]<<" ";
+        // cout<<nums[i]<<" ";
         umap[nums[i]]=i;
         freq[nums[i]]++;
     }
     cout<<endl<<endl;
-    cout<<"freq: "<<freq.count(target-nums[1])<<endl;
-    cout<<"umap: "<<umap.at(target-nums[1])<<endl;
+    // cout<<"freq: "<<freq.count(target-nums[1])<<endl;
+    // cout<<"umap: "<<umap.at(target-nums[1])<<endl;
     for(int i=0;i<nums.size();i++){
-        cout<<"i: "<<i<<endl;
+        // cout<<"i: "<<i<<endl;
         int required=target-nums[i];
         freq[nums[i]]--;
-        cout<<"required: "<<required<<endl;
-        cout<<"freq: " <<freq.count(required)<<endl;
+        // cout<<"required: "<<required<<endl;
+        // cout<<"freq: " <<freq.count(required)<<endl;
         if(freq[required]>=1){
             ans[0]=i;
             ans[1]=umap.at(required);
