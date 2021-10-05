a4f78dc9543b0c6eb60e1b302a83a885b14f54e3
Durgesh Pushpakar <56768530+durgeshpushpakar@users.noreply.github.com>
2021-10-05 18:48:51 +0530

    Update maximumDepth.cpp

--- Trees/maximumDepth.cpp
+++ Trees/maximumDepth.cpp
@@ -3,7 +3,10 @@
 using namespace std;
 int maxDepth(TreeNode* root) {
     if(root==NULL)return 0;
-    
+    if(root->left==NULL && root->right==NULL)return 1;
+    int smallAns1=maxDepth(root->left);
+    int smallAns2=maxDepth(root->right);
+    return max(smallAns1, smallAns2)+1;
 }
 int main()
 {
