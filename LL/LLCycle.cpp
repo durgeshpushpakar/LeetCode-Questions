1e6fcd20361a1e53acbfa1960d92e27b27abfb29
Durgesh Pushpakar <56768530+durgeshpushpakar@users.noreply.github.com>
2021-10-11 17:22:29 +0530

    Update LLCycle.cpp

--- LL/LLCycle.cpp
+++ LL/LLCycle.cpp
@@ -12,6 +12,24 @@ bool hasCycle(ListNode *head) {
     }   
     return false;
 }
+ListNode *detectCycle(ListNode *head) {
+        if (!head || !head->next)
+            return nullptr;
+        ListNode *fast = head, *slow = head;
+        while(fast && fast->next && slow) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) {
+                fast = head;
+                while (fast != slow) {                    
+                    fast = fast->next;
+                    slow = slow->next;
+                }
+                return slow;
+            }
+        }
+        return nullptr;
+}
 int main(){
 
     return 0;
