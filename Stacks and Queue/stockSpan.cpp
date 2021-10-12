41c49650f4e7eb06024749236ef446f4acdbccc5
Durgesh <durgeshpushpakar001@gmail.com>
2021-10-12 15:43:48 +0530

    Update stockSpan.cpp

--- Stacks and Queue/stockSpan.cpp
+++ Stacks and Queue/stockSpan.cpp
@@ -5,20 +5,18 @@ vector <int> calculateSpan(int price[], int n){
     vector<int>ans;
     for(int i=0;i<n;i++){
         if(s.empty()){
-            s.push(i);
             ans.push_back(1);
+            s.push(i);
         }
-        else if(price[i]<price[s.top()]){
-            s.push(i);
+        if(price[i]<price[s.top()]){
             ans.push_back(1);
+            s.push(i);
         }
         else{
-            while(!s.empty() && price[i]>price[s.top()]){
-                s.pop();
-            }
+            while(!s.empty() && price[i]>=price[s.top()])s.pop();
             if(s.empty()){
-                s.push(i);
                 ans.push_back(i+1);
+                s.push(price[i]);
             }
             else{
                 ans.push_back(i-s.top());
