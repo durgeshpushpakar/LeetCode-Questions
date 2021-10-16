9c28e7eeb0cf4b4b8ab84d8b4be1dac0d1ade468
Durgesh <durgeshpushpakar001@gmail.com>
2021-10-16 15:38:33 +0530

    Update stockBuyAndSell.cpp

--- Arrays/stockBuyAndSell.cpp
+++ Arrays/stockBuyAndSell.cpp
@@ -24,6 +24,18 @@ int maxProfit(vector<int>& prices) {
     sum+=Profit;
     return sum;
 }
+vector<vector<int> > stockBuySell(vector<int> arr, int n){
+    vector<vector<int>>ans;
+    int i=0;
+    while(i<=n-1){
+        while(i<n-1 && arr[i+1]<=arr[i])i++;
+        if(i==n-1)break;
+        int temp1=i++;
+        while(arr[i]>=arr[i-1])i++;
+        ans.push_back({temp1, i-1});
+    }
+    return ans;
+}
 int main(){
 
     return 0;
