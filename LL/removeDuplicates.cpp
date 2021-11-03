40b7106984f38f66f2db64b9ebebbb8bcda8544b
Durgesh Pushpakar <durgeshpushpakar001@gmail.com>
2021-11-03 17:52:31 +0530

    Update removeDuplicates.cpp

--- LL/removeDuplicates.cpp
+++ LL/removeDuplicates.cpp
@@ -1,3 +1,7 @@
+// 1 1 2 3 3 4 4 5
+// testcases to be taken care of
+// 1 1 1
+// 1 1 2 2
 #include<bits/stdc++.h>
 #include "listnode.h"
 using namespace std;
@@ -6,14 +10,25 @@ public:
 ListNode* deleteDuplicates(ListNode* head) {
     ListNode*temp=head, *prev=NULL;
     while(temp!=NULL && temp->next!=NULL){
-        while(temp->val==temp->next->val){
-            temp=temp->next;
-        }
-        if(prev==NULL){
-            head=temp->next;
-        }
-        else{
-            prev->next=temp->next;
+        if(temp->val==temp->next->val){
+            while(temp!=NULL && temp->next!=NULL && temp->next->val==temp->val)temp=temp->next;
+            if(temp->next==NULL){
+                if(prev==NULL)return NULL;
+                else{
+                    prev->next=NULL;
+                    return head;
+                }
+            }
+            else if(prev==NULL){
+                head=temp->next;
+                temp=temp->next;
+                continue;
+            }
+            else{
+                prev->next=temp->next;
+                temp=temp->next;
+                continue;
+            }            
         }
         prev=temp;
         temp=temp->next;
